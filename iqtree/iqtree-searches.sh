17402c93b8409a1f5e0b4e39f72ef22667097c44
npicciani <picciani@mmg-class-mac.local>
2016-10-24 10:32:54 -0700

    iqtree search
    script used in maximum likelihood analysis

---
+++ iqtree/iqtree-searches.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+cd /Users/picciani/Documents
+
+cd /Users/picciani/Documents/iqtree-1
+#iqtree-search-1 
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-1/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.3 -nt 2 -bb 1000 -nm 5000 -pre rep1
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-1/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.3 -nt 2 -bb 1000 -nm 5000 -pre rep2
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-1/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.3 -nt 2 -bb 1000 -nm 5000 -pre rep3
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-1/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.3 -nt 2 -bb 1000 -nm 5000 -pre rep4
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-1/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.3 -nt 2 -bb 1000 -nm 5000 -pre rep5
+
+cd /Users/picciani/Documents/iqtree-2
+#iqtree-search-2
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-2/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.4 -nt 2 -bb 1000 -nm 5000 -pre rep1
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-2/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.4 -nt 2 -bb 1000 -nm 5000 -pre rep2
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-2/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.4 -nt 2 -bb 1000 -nm 5000 -pre rep3
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-2/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.4 -nt 2 -bb 1000 -nm 5000 -pre rep4
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-2/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.4 -nt 2 -bb 1000 -nm 5000 -pre rep5
+
+
+cd /Users/picciani/Documents/iqtree-3
+#iqtree-search-3 
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-3/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.6 -nt 2 -bb 1000 -nm 5000 -pre rep1
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-3/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.6 -nt 2 -bb 1000 -nm 5000 -pre rep2
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-3/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.6 -nt 2 -bb 1000 -nm 5000 -pre rep3
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-3/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.6 -nt 2 -bb 1000 -nm 5000 -pre rep4
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-3/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.6 -nt 2 -bb 1000 -nm 5000 -pre rep5
+
+cd /Users/picciani/Documents/iqtree-4
+#iqtree-search-4  
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-4/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.8 -nt 2 -bb 1000 -nm 5000 -pre rep1
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-4/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.8 -nt 2 -bb 1000 -nm 5000 -pre rep2
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-4/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.8 -nt 2 -bb 1000 -nm 5000 -pre rep3
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-4/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.8 -nt 2 -bb 1000 -nm 5000 -pre rep4
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-4/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -pers 0.8 -nt 2 -bb 1000 -nm 5000 -pre rep5
+
+cd /Users/picciani/Documents/iqtree-5
+#iqtree-search-5 
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-5/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 200 -nt 2 -bb 1000 -nm 5000 -pre rep1
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-5/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 200 -nt 2 -bb 1000 -nm 5000 -pre rep2
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-5/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 200 -nt 2 -bb 1000 -nm 5000 -pre rep3
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-5/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 200 -nt 2 -bb 1000 -nm 5000 -pre rep4
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-5/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 200 -nt 2 -bb 1000 -nm 5000 -pre rep5
+
+cd /Users/picciani/Documents/iqtree-6
+#iqtree-search-6  
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-6/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 400 -nt 2 -bb 1000 -nm 5000 -pre rep1
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-6/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 400 -nt 2 -bb 1000 -nm 5000 -pre rep2
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-6/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 400 -nt 2 -bb 1000 -nm 5000 -pre rep3
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-6/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 400 -nt 2 -bb 1000 -nm 5000 -pre rep4
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-6/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 400 -nt 2 -bb 1000 -nm 5000 -pre rep5
+
+cd /Users/picciani/Documents/iqtree-7
+#iqtree-search-7  
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-7/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 600 -nt 2 -bb 1000 -nm 5000 -pre rep1 
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-7/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 600 -nt 2 -bb 1000 -nm 5000 -pre rep2
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-7/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 600 -nt 2 -bb 1000 -nm 5000 -pre rep3
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-7/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 600 -nt 2 -bb 1000 -nm 5000 -pre rep4
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-7/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -numstop 600 -nt 2 -bb 1000 -nm 5000 -pre rep5
+
+cd /Users/picciani/Documents/iqtree-8
+#iqtree-search-8  
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-8/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -nt 2 -bb 1000 -nm 5000 -pre rep1
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-8/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -nt 2 -bb 1000 -nm 5000 -pre rep2
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-8/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -nt 2 -bb 1000 -nm 5000 -pre rep3
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-8/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -nt 2 -bb 1000 -nm 5000 -pre rep4
+/Applications/iqtree-omp -s /Users/picciani/Documents/iqtree-8/all.fasta.muscle-trimmed.fasta -m TIM2+G4 -nt 2 -bb 1000 -nm 5000 -pre rep5
+
+
